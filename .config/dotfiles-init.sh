e44f21efd791a8e5e845c3c415ab48229256bb04
kris fiction <krisfiction@gmail.com>
2024-06-30 19:33:16 -0400

    edit dotfiles-init.sh

--- .config/dotfiles-init.sh
+++ .config/dotfiles-init.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-git clone --bare git@github.com:krisfiction/dotfiles.git $HOME/.gitdotfiles
-# define dotfiles alias locally since the dotfiles
-# aren't installed on the system yet
+git clone --barehttps://github.com/krisfiction/dotfiles.git $HOME/.gitdotfiles
+
+# define dotfiles alias
 function dotfiles {
    git --git-dir=$HOME/.gitdotfiles/ --work-tree=$HOME $@
 }
+
 # create a directory to backup existing dotfiles to
 mkdir -p .gitdotfiles-backup
 dotfiles checkout
@@ -15,6 +16,7 @@ if [ $? = 0 ]; then
     echo "Moving existing dotfiles to ~/.gitdotfiles-backup";
     dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .gitdotfiles-backup/{}
 fi
+
 # checkout dotfiles from repo
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
