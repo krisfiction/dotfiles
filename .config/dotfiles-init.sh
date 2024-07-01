e3e7eb0807f653da2c318d33b4295489ddd8940c
kris fiction <krisfiction@gmail.com>
2024-07-01 11:21:50 -0400

    Update dotfiles-init.sh

--- .config/dotfiles-init.sh
+++ .config/dotfiles-init.sh
@@ -14,8 +14,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles from github.com/krisfiction/dotfiles.git";
   else
     echo "Moving existing dotfiles to ~/.gitdotfiles-backup";
-    #dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .gitdotfiles-backup/{}
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \ xargs -I{} mv {} .gitdotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .gitdotfiles-backup/{}
 fi
 
 # checkout dotfiles from repo
