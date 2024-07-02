a200aff7bb5abac21cd9fde1874421786119af1e
kris fiction <krisfiction@gmail.com>
2024-07-01 20:52:21 -0400

    Update dotfiles-init.sh

--- .config/dotfiles-init.sh
+++ .config/dotfiles-init.sh
@@ -7,6 +7,14 @@ function dotfiles {
    git --git-dir=$HOME/.gitdotfiles/ --work-tree=$HOME $@
 }
 
+# mvp = move + create parents
+function mvp () {
+    source="$1"
+    target="$2"
+    target_dir="$(dirname "$target")"
+    mkdir --parents $target_dir; mv $source $target
+}
+
 # create a directory to backup existing dotfiles to
 mkdir -p .gitdotfiles-backup
 dotfiles checkout
@@ -14,7 +22,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles from github.com/krisfiction/dotfiles.git";
   else
     echo "Moving existing dotfiles to ~/.gitdotfiles-backup";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .gitdotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mvp {} .gitdotfiles-backup/{}
 fi
 
 # checkout dotfiles from repo
